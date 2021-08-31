3fb84a1dc4158d1f29589e9aeb109aeb920d01a5
Stephen Johnson <stephenjayyy@gmail.com>
2021-08-31 17:07:13 -0500

    Adds SimpleArray test

---
+++ EssentialTraining/SimpleArray.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EssentialTraining
+{
+    public class SimpleArray
+    {   
+        public string[] GroceryList;
+
+        public SimpleArray()
+        {
+            GroceryList = new string[4] { "bread", "milk", "eggs", "cheese" };
+        }
+        public override string ToString()
+        {
+            return "There are " + GroceryList.Length + " and they are: " + GroceryList.ToString();
+        }
+    }
+}
