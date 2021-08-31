5cb2530b7d0d243b938ac6e4402aa36cd9c3c203
Stephen Johnson <stephenjayyy@gmail.com>
2021-08-31 17:15:27 -0500

    Adds Awesome Sauce class and test

---
+++ EssentialTrainingTests/AwesomeSauceTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using EssentialTraining;
+
+namespace EssentialTrainingTests
+{
+    [TestClass]
+    public class AwesomeSauceTest
+    {
+        [TestMethod]
+        public void IsSauceAwesomeTest()
+        {
+            var testInstance = new AwesomeSauce();
+            testInstance.Sauces.Add("Tobasco");
+            testInstance.Sauces.Add("Cholula");
+            testInstance.Sauces.Add("Trailer Trash");
+
+            //expect to pass
+            Assert.IsTrue(testInstance.IsSauceAwesome("Trailer Trash"));
+
+            //expect false
+            Assert.IsTrue(testInstance.IsSauceAwesome("Ketchup"));
+        }
+    }
+}
