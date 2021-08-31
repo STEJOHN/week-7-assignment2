96afdcc93bc17882e33ca2d57bb0a1f599f52e40
Stephen Johnson <stephenjayyy@gmail.com>
2021-08-31 17:56:05 -0500

    Adds Programming loops class

---
+++ EssentialTraining/ProgrammingLoops.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EssentialTraining
+{
+    public class ProgrammingLoops
+    {
+        public void ForLoop()
+        {
+            var sum = 0;
+            for (var i = 0; i < 100; i++)
+            {
+                sum = sum + 1;
+            }
+            return;
+        }
+    }
+}
